9a35baf2aac9a62984bddd51601262f201108413
Alessandra <savi.souza@escola.pr.gov.br>
2025-09-09 08:46:39 -0300

    09/09

--- Perguntas.js
+++ Perguntas.js
@@ -4,3 +4,44 @@ criaCartao(
     'resposta'
 )
 
+criaCartao(
+    'Programação',
+    'Qual é o autor de "Dom Quixote"?',
+    'Miguel de Cervantes'
+)
+
+criaCartao(
+    'Programação',
+    'Quem foi o líder da Revolução Russa de 1917?',
+    'Vladimir Lenin'
+)
+
+criaCartao(
+    'Programação',
+    'Qual foi o primeiro satélite lançado para a órbita terrestre?',
+    'Sputnik 1'
+)
+
+criaCartao(
+    'Programação',
+    'Qual país tem o maior número de idiomas oficiais?',
+    'Índia'
+)
+
+criaCartao(
+    'Programação',
+    'Quantos elementos químicos estão presente na tabela periódica?',
+    '118 elementos'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é o maior país da África?',
+    'Argélia'
+)
+
+criaCartao(
+    'Programação',
+    'Qual civilização antiga foi responsável pela invenção do concreto?',
+    'Romanos'
+)
