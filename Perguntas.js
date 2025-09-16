ee4f891ab41a0eca70ef03f441d2dd1be73aec8e
Alessandra <savi.souza@escola.pr.gov.br>
2025-09-16 09:18:47 -0300

    16/09

--- Perguntas.js
+++ Perguntas.js
@@ -5,43 +5,61 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
+    'Literatura',
     'Qual é o autor de "Dom Quixote"?',
     'Miguel de Cervantes'
 )
 
 criaCartao(
-    'Programação',
+    'Hitória',
     'Quem foi o líder da Revolução Russa de 1917?',
     'Vladimir Lenin'
 )
 
 criaCartao(
-    'Programação',
+    'Conhecimentos Gerais',
     'Qual foi o primeiro satélite lançado para a órbita terrestre?',
     'Sputnik 1'
 )
 
 criaCartao(
-    'Programação',
+    'Conhecimentos Gerais',
     'Qual país tem o maior número de idiomas oficiais?',
     'Índia'
 )
 
 criaCartao(
-    'Programação',
+    'Química',
     'Quantos elementos químicos estão presente na tabela periódica?',
     '118 elementos'
 )
 
 criaCartao(
-    'Programação',
+    'Geografia',
     'Qual é o maior país da África?',
     'Argélia'
 )
 
 criaCartao(
-    'Programação',
+    'Curiosidades',
     'Qual civilização antiga foi responsável pela invenção do concreto?',
     'Romanos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o ponto mais profundo conhecido nos oceanos?',
+    'Fossa das Marianas'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Qual país foi o primeiro a dar direito de voto às mulheres?',
+    'Nova Zelândia'
+)
+
+criaCartao(
+    'Curioidades',
+    'Qual animal é capaz de dormir com metade do cérebro acordada?',
+    'Golfinho'
 )
