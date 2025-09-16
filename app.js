a342441e88ec195bc4d0d83e0617c7cb019c634a
Alessandra <savi.souza@escola.pr.gov.br>
2025-09-16 10:47:33 -0300

    16/09

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article');
     cartao.className = 'cartao'
 
-cartao.innerHTML=`
+    cartao.innerHTML = `
 <div class="cartao__conteudo">
 <h3>${categoria}</h3>
 <div class="cartao__conteudo__pergunta">
@@ -13,6 +13,14 @@ cartao.innerHTML=`
 <p> ${resposta}</p>
 </div>
 </div>`
+
+    let respotaEstaVisivel = false
+
+    function viraCartao(){
+    respotaEstaVisivel=!respotaEstaVisivel
+    }
+
+
 
 container.appendChild(cartao)
 
